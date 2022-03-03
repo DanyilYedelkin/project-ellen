9b41686629e2214d792f9a2fb05f91fc9a1fb848
Danyil Yedelkin <79505924+DanyilYedelkin@users.noreply.github.com>
2022-03-03 10:44:54 +0100

    Update Wrench.java

--- src/main/java/sk/tuke/kpi/oop/game/items/Wrench.java
+++ src/main/java/sk/tuke/kpi/oop/game/items/Wrench.java
@@ -1,22 +1,13 @@
 package sk.tuke.kpi.oop.game.items;
 
-/* (the code is not perfect, later, it will be modified and improved) */
-
-//add libraries
-//import sk.tuke.kpi.gamelib.actions.When;
 import sk.tuke.kpi.gamelib.graphics.Animation;
 import sk.tuke.kpi.oop.game.DefectiveLight;
-//import sk.tuke.kpi.oop.game.Reactor;
-//import sk.tuke.kpi.oop.game.tools.BreakableTool;
+
 
 public class Wrench extends BreakableTool<DefectiveLight> implements Collectible{
-    //private Animation wrenchAnimation;
 
     public Wrench() {
         super(2);
-
-        //wrenchAnimation = new Animation("sprites/wrench.png", 16, 16, 0.1f);
-        //setAnimation(wrenchAnimation);
         setAnimation(new Animation("sprites/wrench.png", 16, 16, 0.1f));
     }
 
