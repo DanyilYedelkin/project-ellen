cbb2ed6f989255e46c3d112b4b159aea2baf56ee
Danyil Yedelkin <79505924+DanyilYedelkin@users.noreply.github.com>
2022-03-03 10:43:59 +0100

    Update Hammer.java

--- src/main/java/sk/tuke/kpi/oop/game/items/Hammer.java
+++ src/main/java/sk/tuke/kpi/oop/game/items/Hammer.java
@@ -1,15 +1,10 @@
 package sk.tuke.kpi.oop.game.items;
 
-/* (the code is not perfect, later, it will be modified and improved) */
-
 //add libraries
 import sk.tuke.kpi.gamelib.graphics.Animation;
-//import sk.tuke.kpi.oop.game.Reactor;
 import sk.tuke.kpi.oop.game.Repairable;
-//import sk.tuke.kpi.oop.game.tools.BreakableTool;
 
 public class Hammer extends BreakableTool<Repairable> implements Collectible{
-    //private Animation hammerAnimation;
     private int healthUse;       // a variable, that have a health point of the hammer
 
 
@@ -17,10 +12,7 @@ public class Hammer extends BreakableTool<Repairable> implements Collectible{
         super(1);
         this.healthUse = 1;        // health point of a regular hammer
         setRemainingUses(1);
-        // create animation object
-        //hammerAnimation = new Animation("sprites/hammer.png", 16, 16);
-        // set actor's animation to just created Animation object
-        //setAnimation(hammerAnimation);
+
         setAnimation(new Animation("sprites/hammer.png", 16, 16));
     }
 
