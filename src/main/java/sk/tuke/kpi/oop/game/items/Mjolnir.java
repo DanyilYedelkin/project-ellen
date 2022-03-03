3951bb5f18303080f3fabb8664ba9055337da6a4
Danyil Yedelkin <79505924+DanyilYedelkin@users.noreply.github.com>
2022-03-03 10:44:24 +0100

    Update Mjolnir.java

--- src/main/java/sk/tuke/kpi/oop/game/items/Mjolnir.java
+++ src/main/java/sk/tuke/kpi/oop/game/items/Mjolnir.java
@@ -1,22 +1,14 @@
 package sk.tuke.kpi.oop.game.items;
 
-/* (the code is not perfect, later, it will be modified and improved) */
-
-//add libraries
 import sk.tuke.kpi.gamelib.graphics.Animation;
-//import sk.tuke.kpi.oop.game.tools.Hammer;
+
 
 public class Mjolnir extends Hammer {
-    //private Animation mjolnirAnimation;
 
     public Mjolnir() {
         super();
         setRemainingUses(4);
 
-        /* create an animation for mjolnir (a hammer, that have 4 health points) */
-        //mjolnirAnimation = new Animation("sprites/hammer.png", 16, 16);
-        // set actor's animation to just created Animation object
-        //setAnimation(mjolnirAnimation);
         setAnimation(new Animation("sprites/hammer.png", 16, 16));
     }
 }
