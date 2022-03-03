2c9244647535056d38052ca83a1a114f6b3396ad
Danyil Yedelkin <79505924+DanyilYedelkin@users.noreply.github.com>
2022-03-03 10:56:15 +0100

    Update Main.java

--- src/main/java/sk/tuke/kpi/oop/game/Main.java
+++ src/main/java/sk/tuke/kpi/oop/game/Main.java
@@ -2,11 +2,7 @@ package sk.tuke.kpi.oop.game;
 
 import sk.tuke.kpi.gamelib.*;
 import sk.tuke.kpi.gamelib.backends.lwjgl.LwjglBackend;
-//import sk.tuke.kpi.oop.game.scenarios.FirstSteps;
 import sk.tuke.kpi.oop.game.scenarios.DieHard;
-//import sk.tuke.kpi.oop.game.scenarios.EscapeRoom;
-//import sk.tuke.kpi.oop.game.scenarios.MissionImpossible;
-
 
 public class Main {
     public static void main(String[] args) {
@@ -18,18 +14,11 @@ public class Main {
         Game game = new GameApplication(windowSetup, new LwjglBackend()); // in case of Mac OS system use "new Lwjgl2Backend()" as the second parameter
 
         // creating scene for game
-        // using class `World` as implementation of interface `Scene`
-        //Scene scene  = new World("World", "maps/mission-impossible.tmx", new MissionImpossible.Factory());
-        //Scene scene  = new World("World", "maps/escape-room.tmx", new EscapeRoom.Factory());
         Scene scene  = new World("World", "maps/die-hard.tmx", new DieHard.Factory());
 
         // adding scene into the game
         game.addScene(scene);
 
-        //MissionImpossible listener = new MissionImpossible();
-        //scene.addListener(listener);
-
-        //EscapeRoom listener = new EscapeRoom();
         DieHard listener = new DieHard();
         scene.addListener(listener);
 
